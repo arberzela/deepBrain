6e15fb35f2ef2fb9af94fd8be6a8e44f127a632f
risufaj <ralvi.isufaj@gmail.com>
2017-03-07 11:57:09 +0100

    Update Word2Gauss.py

--- Word2Gauss.py
+++ Word2Gauss.py
@@ -4,138 +4,12 @@ import theano.tensor as T
 from itertools import islice
 import re
 from theano import function
+import Vocabulary
 
 LARGEST_UINT32 = 4294967295
 DTYPE = np.float32
 
 
-def tokenizer(s):
-    return s.strip().split()
-
-
-def create_vocabulary(corpus):
-    with open(corpus, encoding='utf-8') as f:
-        words = set()
-        lines_passed = 0
-        id = 0
-        vocabulary = {}
-        for line in f:
-            if lines_passed >= 1000:
-                break
-            # get only letters
-            line = re.sub('[^A-Za-zÄäÖöÜüß]+', ' ', line)
-            line = line.lower()
-            words.update(line.split())
-            lines_passed += 1
-
-        for word in words:
-            vocabulary[word] = id
-            id += 1
-
-        return vocabulary
-
-
-def text_to_pairs(text, random_gen, half_window_size=2, nsamples_per_word=1):
-    npairs = sum([2 * len(doc) * half_window_size * nsamples_per_word for doc in text])
-    pairs = np.empty((npairs, 5), dtype=np.uint32)
-    randids = random_gen(npairs)
-    next_pair = 0
-    for doc in text:
-        cdoc = doc
-        doc_len = cdoc.shape[0]
-        for i in range(doc_len):
-            if cdoc[i] == LARGEST_UINT32:
-                continue
-            for j in range(i + 1, min(i+ half_window_size + 1, doc_len)):
-                if cdoc[j] == LARGEST_UINT32:
-                    continue
-                for k in range(nsample_per_word):
-                    pairs[next_pair, 0] = cdoc[i]
-                    pairs[next_pair, 1] = cdoc[j]
-                    pairs[next_pair, 2] = cdoc[i]
-                    pairs[next_pair, 3] = randids[next_pair]
-                    pairs[next_pair, 4] = 0
-                    next_pair += 1
-
-                    pairs[next_pair, 0] = cdoc[i]
-                    pairs[next_pair, 1] = cdoc[j]
-                    pairs[next_pair, 2] = randids[next_pair]
-                    pairs[next_pair, 3] = cdoc[j]
-                    pairs[next_pair, 4] = 1
-                    next_pair += 1
-    return np.ascontiguousarray(pairs[:next_pair, :])
-
-
-class Vocabulary(object):
-    def __init__(self, corpus, tokenizer=tokenizer):
-        '''
-        tokens: a {'token1': 0, 'token2': 1, ...} map of token -> id
-            the ids must run from 0 to n_tokens - 1 inclusive
-        tokenizer: accepts a string and returns a list of strings
-        TODO: Think how to transform a corpus into tokens, unique words perhaps?
-    '''
-
-        self._tokens = create_vocabulary(corpus)
-        self._ids = {i: token for token, i in self._tokens.items()}
-        self._ntokens = len(self._tokens)
-        self._tokenizer = tokenizer
-
-    def word2id(self, word):
-        if word in self._tokens:
-            return self._tokens[word]
-        else:
-            return None
-
-    def numberOfTokens(self):
-        return self._ntokens  
-      
-    def id2word(self, id):
-        if id in self._ids:
-            return self._ids[id]
-        else:
-            return None
-
-    def tokenize(self, s):
-        '''
-        Removes OOV tokens using built
-        '''
-        tokens = self._tokenizer(s)
-        return [token for token in tokens if token in self._tokens]
-
-    def tokenize_ids(self, s, remove_oov=True):
-        tokens = self._tokenizer(s)
-        if remove_oov:
-            return np.array([self.word2id(token)
-                             for token in tokens if token in self._tokens],
-                            dtype=np.uint32)
-
-        else:
-            ret = np.zeros(len(tokens), dtype=np.uint32)
-            for k, token in enumerate(tokens):
-                try:
-                    ret[k] = self.word2id(token)
-                except KeyError:
-                    ret[k] = LARGEST_UINT32
-            return ret
-
-    def random_ids(self, num):
-        return np.random.randint(0, self._ntokens, size=num).astype(np.uint32)
-
-    def iter_pairs(self, fin, batch_size=10, nsamples=2, window=5):
-
-        documents = iter(fin)
-        batch = list(islice(documents, batch_size))
-        while len(batch) > 0:
-            text = [
-                vocab.tokenize_ids(doc, remove_oov=True)
-                for doc in batch
-                ]
-            pairs = text_to_pairs(text, self.random_ids,
-                                  nsamples_per_word=nsamples,
-                                  half_window_size=window)
-            yield pairs
-            batch = list(islice(documents, batch_size))
-            #return pairs
 
 
 class GaussianDistribution(object):
@@ -156,7 +30,6 @@ class GaussianDistribution(object):
         self.Gaussian = np.concatenate((self.mu, self.Sigma), axis=1)
 
     def energy(self, i, j):
-
         # TensorVariables for mi, mj, si, sj respectivelly.
         a, b = T.fvectors('a', 'b')
         c, d = T.fscalars('c', 'd')
@@ -172,30 +45,30 @@ class GaussianDistribution(object):
         # TensorVariables for mi, mj, si, sj respectivelly.
         a, b = T.fvectors('a', 'b')
         c, d = T.fscalars('c', 'd')
-        
+
         # Energy as a TensorVariable
         E = -0.5 * (self.K * d / c + T.sum((a - b) ** 2 / c) - self.K - self.K * T.log(d / c))
-        
-        g1 = T.grad(E, a) # dE/dmi
+
+        g1 = T.grad(E, a)  # dE/dmi
         f1 = function([a, b, c, d], g1)
-        
-        g2 = T.grad(E, b) # dE/dmj
+
+        g2 = T.grad(E, b)  # dE/dmj
         f2 = function([a, b, c, d], g2)
-        
-        g3 = T.grad(E, c) # dE/dsi
+
+        g3 = T.grad(E, c)  # dE/dsi
         f3 = function([a, b, c, d], g3)
-        
-        g4 = T.grad(E, d) # dE/dsj
+
+        g4 = T.grad(E, d)  # dE/dsj
         f4 = function([a, b, c, d], g4)
 
         grad[0][:-1] = f1(self.mu[i], self.mu[j], float(self.Sigma[i]), float(self.Sigma[j]))
         grad[1][:-1] = f2(self.mu[i], self.mu[j], float(self.Sigma[i]), float(self.Sigma[j]))
-        grad[0,-1] = f3(self.mu[i], self.mu[j], float(self.Sigma[i]), float(self.Sigma[j]))
-        grad[1,-1] = f4(self.mu[i], self.mu[j], float(self.Sigma[i]), float(self.Sigma[j]))
-        
+        grad[0, -1] = f3(self.mu[i], self.mu[j], float(self.Sigma[i]), float(self.Sigma[j]))
+        grad[1, -1] = f4(self.mu[i], self.mu[j], float(self.Sigma[i]), float(self.Sigma[j]))
+
         return grad
 
-    
+
 class GaussianEmbedding(object):
     def __init__(self, N, size=100, covariance_type='spherical',
                  energy='KL', C=1.0, m=0.1, M=10.0, Closs=1.0, eta=1.0):
@@ -211,32 +84,33 @@ class GaussianEmbedding(object):
         self.grad_mu = theano.shared(np.zeros_like(self.dist.mu))
         self.grad_sigma = theano.shared(np.zeros_like(self.dist.Sigma))
 
-    #def loss(self, pos, neg):
-     #   return max(0.0,
-      #             self.Closs - self.energy.energy(*pos) + self.energy.energy(*neg)
-       #            )
-    def loss(self,posEng,negEng):
+        # def loss(self, pos, neg):
+        #   return max(0.0,
+        #             self.Closs - self.energy.energy(*pos) + self.energy.energy(*neg)
+        #            )
+
+    def loss(self, posEng, negEng):
         return max(
             0.0,
             self.Closs - posEng + negEng
         )
 
-    def train(self,pairs):
-        #pairs : (i_pos,j_pos) (i_neg,j_neg). comes from text_to_pairs
+    def train(self, pairs):
+        # pairs : (i_pos,j_pos) (i_neg,j_neg). comes from text_to_pairs
         posFac = -1.0
         negFac = 1.0
-        for pos,neg in pairs:
+        for pos, neg in pairs:
 
-            #if loss for this case is 0, there's nothing to update
+            # if loss for this case is 0, there's nothing to update
             if self.loss(self.dist.energy(*pos), self.dist.energy(*neg)) < 1e-14:
                 continue
 
-            #update positive samples
+            # update positive samples
             posGrad = self.dist.gradient(*pos)
-            self.update(posGrad[0],self.eta,posFac,pos[0])
-            self.update(posGrad[1],self.eta,posFac,pos[1])
+            self.update(posGrad[0], self.eta, posFac, pos[0])
+            self.update(posGrad[1], self.eta, posFac, pos[1])
 
-            #update negative samples
+            # update negative samples
             negGrad = self.dist.gradient(*neg)
             self.update(negGrad[0], self.eta, negFac, neg[0])
             self.update(negGrad[1], self.eta, negFac, neg[1])
@@ -244,75 +118,76 @@ class GaussianEmbedding(object):
     def update(self, gradients, eta, fac, k):
         # accumulate mu
         val = self._acc_grad_mu[k]
-        val += np.sum(gradients[:-1]**2)/len(gradients[:-1])
+        val += np.sum(gradients[:-1] ** 2) / len(gradients[:-1])
         self._acc_grad_mu[k] = val
 
-        #val = self.acc_grad_mu[k].get_value()
-        #val += np.sum(gradients[:-1] ** 2) / len(gradients[:-1])
-        #self.acc_mu_grad_mu[k].set_value(val)
+        # val = self.acc_grad_mu[k].get_value()
+        # val += np.sum(gradients[:-1] ** 2) / len(gradients[:-1])
+        # self.acc_mu_grad_mu[k].set_value(val)
 
         # accumulate sigma
         val = self._acc_grad_sigma[k]
-        val += gradients[-1]**2
-        self._acc_grad_sigma[k]=val
+        val += gradients[-1] ** 2
+        self._acc_grad_sigma[k] = val
 
-        #val = self._acc_grad_sigma.get_value()
-        #val += gradients[-1] ** 2
-        #self._acc_grad_sigma.set_value(val)
+        # val = self._acc_grad_sigma.get_value()
+        # val += gradients[-1] ** 2
+        # self._acc_grad_sigma.set_value(val)
 
         # updates
         # update mu
         val = self.grad_mu[k]
         eta_mu = eta / np.sqrt(self._acc_grad_mu[k] + 1.0)
-        updates1 = (self.grad_mu,T.set_subtensor(val, val - (fac*eta_mu*gradients[:-1])))
-        updateFunc1 = function([],updates=[updates1])
+        updates1 = (self.grad_mu, T.set_subtensor(val, val - (fac * eta_mu * gradients[:-1])))
+        updateFunc1 = function([], updates=[updates1])
         updateFunc1()
-        #regularization
+        # regularization
         new_val = self.grad_mu[k]
-        l2_mu = np.sqrt(np.sum(new_val.eval()**2))
+        l2_mu = np.sqrt(np.sum(new_val.eval() ** 2))
         if l2_mu > self.C:
-            updates2 = (self.grad_mu,T.set_subtensor(new_val,new_val*(self.C/l2_mu)))
-            updateFunc2 = function([],updates=[updates2])
+            updates2 = (self.grad_mu, T.set_subtensor(new_val, new_val * (self.C / l2_mu)))
+            updateFunc2 = function([], updates=[updates2])
             updateFunc2()
 
-        #val = self.grad_mu[k].get_value()
-        #val -= fac * eta_mu * gradients[:-1]
-        #self.grad_mu[k].set_value(val)
-        #l2_mu = np.sqrt(np.sum(self.grad_mu[k].get_value() ** 2))
-        #if l2_mu > self.C:
-         #   val = self.grad_mu[k].get_value()
-          #  val *= (self.C / l2_mu)
-           # self.grad_mu[k].set_value(val)
+            # val = self.grad_mu[k].get_value()
+            # val -= fac * eta_mu * gradients[:-1]
+            # self.grad_mu[k].set_value(val)
+            # l2_mu = np.sqrt(np.sum(self.grad_mu[k].get_value() ** 2))
+            # if l2_mu > self.C:
+            #   val = self.grad_mu[k].get_value()
+            #  val *= (self.C / l2_mu)
+            # self.grad_mu[k].set_value(val)
 
         # update sigma
         val = self.grad_sigma[k]
-        eta_sigma = eta/ np.sqrt(self._acc_grad_sigma[k]+1.0)
-        updates1 = (self.grad_sigma,T.set_subtensor(val,val-(fac*eta_sigma*gradients[-1])))
-        updateFunc1 = function([],updates=[updates1])
+        eta_sigma = eta / np.sqrt(self._acc_grad_sigma[k] + 1.0)
+        updates1 = (self.grad_sigma, T.set_subtensor(val, val - (fac * eta_sigma * gradients[-1])))
+        updateFunc1 = function([], updates=[updates1])
         updateFunc1()
-        #regularization
+        # regularization
         new_val = self.grad_sigma[k]
-        updates2 = (self.grad_sigma,T.set_subtensor(new_val,T.maximum(
-            float(self.m),T.minimum(float(self.M,new_val))
+        updates2 = (self.grad_sigma, T.set_subtensor(new_val, T.maximum(
+            float(self.m), T.minimum(float(self.M, new_val))
         )))
-        updateFunc2 = function([],updates=[updates2])
+        updateFunc2 = function([], updates=[updates2])
         updateFunc2()
 
-       # val = self.grad_sigma[k].get_value()
-       # eta_sigma = eta / np.sqrt(val + 1.0)
-       # val -= fac * eta * gradients[-1]
-       # self.grad_sigma[k].set_value(val)
-       # self.grad_sigma[k].set_value(np.maximum(self.m, np.minimum(self.M, val)))
+        # val = self.grad_sigma[k].get_value()
+        # eta_sigma = eta / np.sqrt(val + 1.0)
+        # val -= fac * eta * gradients[-1]
+        # self.grad_sigma[k].set_value(val)
+        # self.grad_sigma[k].set_value(np.maximum(self.m, np.minimum(self.M, val)))
 
-if __name__=="__main__":
-    #change corpus and test path
+
+if __name__ == "__main__":
+    # change corpus and test path
     corpus = '/media/ralvi/0A527FA0527F8F67/Project/test'
     vocab = Vocabulary(corpus)
     print(vocab.numberOfTokens())
     embeddings = GaussianEmbedding(vocab.numberOfTokens())
 
-    #generate the pairs
-    with open('/media/ralvi/0A527FA0527F8F67/Project/test','r') as file:
+    # generate the pairs
+    with open('/media/ralvi/0A527FA0527F8F67/Project/test', 'r') as file:
         pairs = vocab.iter_pairs(file)
         print(pairs)
         for pair in pairs:
