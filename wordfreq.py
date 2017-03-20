ab9e21999e7acc2848492c1355592d01bab3e476
ArlindKadra <arlindkadra@gmail.com>
2017-03-20 17:37:52 +0100

    Updated bug in wordFrequencies

--- wordfreq.py
+++ wordfreq.py
@@ -12,13 +12,12 @@ def separateSentencesIntoWords(sentence):
     words = sentence.split()
     # remove punctuation
     for word in words:
-        translation = str.maketrans("","",string.punctuation)
-        refinedWord = (word.translate(translation)).lower()
+        refinedWord = word.strip(string.punctuation).lower()
         wordFrequency = wordFrequenciesDict.get(refinedWord)
         if(wordFrequency == None):
             wordFrequenciesDict[refinedWord] = 1
         else:
-            wordFrequenciesDict.update({refinedWord:wordFrequency+1})
+            wordFrequenciesDict.update({refinedWord:wordFrequency + 1})
 
 
 
@@ -36,19 +35,17 @@ def wordsToFrequence(fileName):
     rowsData = file.parse("Tabelle1", parse_cols=[indexColumn])
     # iterate through the sentences from the column
     Data = rowsData.get("Hochdeutsch ohne (Vor-)Vorfeld/Nachfeld")
-    for x in range(1, len(Data)):
+    for x in range(0, len(Data)):
         separateSentencesIntoWords(Data[x])
 
-
-
 # get the words and update the frequencies for all patient
-wordsToFrequence("C:\\Users\\Lindarx\\Desktop\\CAR_EEG\\P1\\linguistic_information\\P1_07_09_2016_anonymisiert.xls")
+wordsToFrequence("/media/arlind/Windows/Users/Lindarx/Desktop/CAR_EEG/P1/linguistic_information/P1_07_09_2016_anonymisiert.xls")
 print(len(wordFrequenciesDict))
-wordsToFrequence("C:\\Users\\Lindarx\\Desktop\\CAR_EEG\\P2\\linguistic_information\\P2_07_09_2016_anonymisiert.xls")
+wordsToFrequence("/media/arlind/Windows/Users/Lindarx/Desktop/CAR_EEG/P2/linguistic_information/P2_07_09_2016_anonymisiert.xls")
 print(len(wordFrequenciesDict))
-wordsToFrequence("C:\\Users\\Lindarx\\Desktop\\CAR_EEG\\P3\\linguistic_information\\P3_07_09_2016_anonymisiert.xls")
+wordsToFrequence("/media/arlind/Windows/Users/Lindarx/Desktop/CAR_EEG/P3/linguistic_information/P3_07_09_2016_anonymisiert.xls")
 print(len(wordFrequenciesDict))
-wordsToFrequence("C:\\Users\\Lindarx\\Desktop\\CAR_EEG\\P4\\linguistic_information\\P4_11_10_2016_anonymisiert.xls")
+wordsToFrequence("/media/arlind/Windows/Users/Lindarx/Desktop/CAR_EEG/P4/linguistic_information/P4_11_10_2016_anonymisiert.xls")
 print(len(wordFrequenciesDict))
 print(wordFrequenciesDict)
 
