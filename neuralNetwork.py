840e30c5e5b8152183fd6e4c5da523282b96541e
ArlindKadra <arlindkadra@gmail.com>
2017-04-21 13:55:24 +0200

    Updated network
    Converted each train, validation, test label to a one hot vector.

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,7 +1,14 @@
 import _pickle as Cpickle
+import theano
+import theano.tensor as T
+import lasagne
+import numpy as np
+import time
+
 class NeuralNetwork:
 
-    def create_network(self):
+
+    def build_cnn(self):
         #merr te dhenat
         with open("data.pickle", "r") as file:
             data = Cpickle.load(file)
@@ -10,15 +17,28 @@ class NeuralNetwork:
         X_val, y_val = data[1]
         X_test, y_test = data[2]
 
+        vocabulary = list(set(y_train))
+        vocabulary_length = len(vocabulary)
+        word2vec = {}
+
+        for i in range(0, vocabulary_length):
+            vec = np.zeros(vocabulary_length)
+            vec[i] = 1
+            word2vec[vocabulary[i]] = vec
+
+        y_train = [word2vec[item] for item in y_train]
+        y_val = [word2vec[item] for item in y_val]
+        y_test = [word2vec[item] for item in y_test]
+
 
         input_var = T.tensor4('inputs')
         target_var = T.ivector('targets')
 
-        network = build_cnn(input_var)
+        neuralNetwork = self.create_network(input_var)
 
         # Create a loss expression for training, i.e., a scalar objective we want
         # to minimize (for our multi-class problem, it is the cross-entropy loss):
-        prediction = lasagne.layers.get_output(network)
+        prediction = lasagne.layers.get_output(neuralNetwork)
         loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
         loss = loss.mean()
         # We could add some weight decay as well here, see lasagne.regularization.
@@ -26,17 +46,17 @@ class NeuralNetwork:
         # Create update expressions for training, i.e., how to modify the
         # parameters at each training step. Here, we'll use Stochastic Gradient
         # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
-        params = lasagne.layers.get_all_params(network, trainable=True)
+        params = lasagne.layers.get_all_params(neuralNetwork, trainable=True)
         updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.9)
 
         # Create a loss expression for validation/testing. The crucial difference
         # here is that we do a deterministic forward pass through the network,
         # disabling dropout layers.
-        test_prediction = lasagne.layers.get_output(network, deterministic=True)
+        test_prediction = lasagne.layers.get_output(neuralNetwork, deterministic=True)
         test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
         test_loss = test_loss.mean()
         # As a bonus, also create an expression for the classification accuracy:
-        test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),dtype=theano.config.floatX)
+        test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var), dtype=theano.config.floatX)
 
         # Compile a function performing a training step on a mini-batch (by giving
         # the updates dictionary) and returning the corresponding training loss:
@@ -50,12 +70,12 @@ class NeuralNetwork:
             file.write("Started Training")
 
         # We iterate over epochs:
-        for epoch in range(num_epochs):
+        for epoch in range(10):
             # In each epoch, we do a full pass over the training data:
             train_err = 0
             train_batches = 0
             start_time = time.time()
-            for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
+            for batch in self.generate_batch(X_train, y_train, 500, shuffle=True):
                 inputs, targets = batch
                 train_err += train_fn(inputs, targets)
                 train_batches += 1
@@ -64,7 +84,7 @@ class NeuralNetwork:
             val_err = 0
             val_acc = 0
             val_batches = 0
-            for batch in generate_batch(X_val, y_val, 500, shuffle=False):
+            for batch in self.generate_batch(X_val, y_val, 500, shuffle=False):
                 inputs, targets = batch
                 err, acc = val_fn(inputs, targets)
                 val_err += err
@@ -73,7 +93,7 @@ class NeuralNetwork:
 
             # Then we print the results for this epoch: # Finally, launch the training loop.
             with open("networkInfo.txt", "a") as file:
-                    file.write("Epoch {} of {} took {:.3f}s".format(epoch + 1, num_epochs, time.time() - start_time))
+                    file.write("Epoch {} of {} took {:.3f}s".format(epoch + 1, 10, time.time() - start_time))
                     file.write("  training loss:\t\t{:.6f}".format(train_err / train_batches))
                     file.write("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
                     file.write("  validation accuracy:\t\t{:.2f} %".format(val_acc / val_batches * 100))
@@ -82,7 +102,7 @@ class NeuralNetwork:
         test_err = 0
         test_acc = 0
         test_batches = 0
-        for batch in generate_batch(X_test, y_test, 500, shuffle=False):
+        for batch in self.generate_batch(X_test, y_test, 500, shuffle=False):
             inputs, targets = batch
             err, acc = val_fn(inputs, targets)
             test_err += err
@@ -94,10 +114,11 @@ class NeuralNetwork:
             file.write("  test accuracy:\t\t{:.2f} %".format(test_acc / test_batches * 100))
 
         # Dumping network parameters
-        np.savez('model.npz', *lasagne.layers.get_all_param_values(network))
+        with open("parameters.pickle" , "w") as parameterFile:
+            Cpickle.dump(lasagne.layers.get_all_param_values(neuralNetwork), parameterFile)
 
 
-    def build_cnn(input_var=None):
+    def create_network(input_var=None):
         # Input layer, as usual:
         network = lasagne.layers.InputLayer(shape=(500, 2649, 8, 8), input_var=input_var)
 
@@ -114,9 +135,16 @@ class NeuralNetwork:
         # Softmax
         network = lasagne.layers.DenseLayer(network, num_units= 108, nonlinearity=lasagne.nonlinearities.softmax)
 
-    def generate_batch(data, batch_size=BATCH_SIZE):
+        return network
+
+    def generate_batch(data, batch_size = 500):
         train_set_x = data[0]
         train_set_y = data[1]
         assert(len(train_set_x) == len(train_set_y))
         for i in range(0, len(data[0]) - batch_size + 1, batch_size):
             yield (train_set_x[i:i + batch_size], train_set_y[i:i + batch_size])
+
+if __name__ == '__main__':
+    network = NeuralNetwork()
+
+    network.build_cnn()
