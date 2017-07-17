6ed4e043c1b55d68467e73377543fb43f1405acf
Arber Zela <arber.zela21@gmail.com>
2017-07-17 17:58:24 +0200

    Update deepBrain.py

--- deepBrain/deepBrain.py
+++ deepBrain/deepBrain.py
@@ -1,9 +1,6 @@
 import tensorflow as tf
 import read_TFRec
 #import rnn_cell
-#from helper_routines import _variable_on_cpu
-#from helper_routines import _variable_with_weight_decay
-#from helper_routines import _activation_summary
 
 NUM_CLASSES = read_TFRec.NUM_CLASSES
 
@@ -65,16 +62,15 @@ if FLAGS.checkpoint is not None:
         FLAGS.temporal_stride = params['temporal_stride']
         FLAGS.initial_lr = params['initial_lr']
 
+from util import _variable_on_cpu
+from util import _variable_with_weight_decay
+from util import _activation_summary
         
 def inputs(eval_data, shuffle=False):
     """
     Construct input for the model evaluation using the Reader ops.
 
-    :patientNr: int, patient number
     :eval_data: 'train', 'test' or 'eval'
-    :data_dir: folder containing the pre-processed data
-    :batch_size: int, size of mini-batch
-    :use_fp16: bool, if True use fp16 else fp32
     :shuffle: bool, to shuffle the tfrecords or not. 
     
     :returns:
@@ -97,45 +93,41 @@ def inputs(eval_data, shuffle=False):
     return feats, labels, seq_lens
 
 
-def inference(feats, seq_lens, params):
+def inference(feats, seq_lens):
     '''
     Build the deepBrain model.
 
     :feats: ECoG features returned from inputs().
     :seq_lens: Input sequence length for each utterance.
-    :params: parameters of the model.
 
     :returns: logits.
     '''
-    if params.use_fp16:
-        dtype = tf.float16
-    else:
-        dtype = tf.float32
+    dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
 
     feat_len = feats.get_shape().as_list()[-1]
 
     # convolutional layers
     with tf.variable_scope('conv1') as scope:
         kernel = _variable_with_weight_decay(
-            'wights',
-            shape=[11, feat_len, 1, params.num_filters],
+            'weights',
+            shape=[11, feat_len, 1, FLAGS.num_filters],
             wd_value=None,
-            use_fp16=params.use_fp16)
+            use_fp16=FLAGS.use_fp16)
 
         feats = tf.expand_dims(feats, dim=-1)
         conv = tf.nn.conv2d(feats, kernel,
-                            [1, params.temoral_stride, 1, 1],
+                            [1, FLAGS.temoral_stride, 1, 1],
                              padding='SAME')
 
-        biases = _variable_on_cpu('biases', [params.num_filters],
+        biases = _variable_on_cpu('biases', [FLAGS.num_filters],
                                   tf.constant_initializer(-0.05),
-                                  params.use_fp16)
+                                  FLAGS.use_fp16)
         
         bias = tf.nn.bias_add(conv, biases)
         conv1 = tf.nn.relu(bias, name=scope.name)
         _activation_summary(conv1)
 
         # dropout
-        conv1_drop = tf.nn.dropout(conv1, params.keep_prob)
+        conv1_drop = tf.nn.dropout(conv1, FLAGS.keep_prob)
 
     # recurrent layer
